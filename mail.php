02fb0873d475ef34c520da39f71af91b8f1dc76f
Maria Cristina Di Termine <mariacristinadt@gmail.com>
2015-07-22 10:14:04 +0200

    Cleaned the folder from file we don't use, added a title on the html, fixed mail.php

--- mail.php
+++ mail.php
@@ -7,6 +7,7 @@
 $mailSuccess = null;
 $mailFailed = null;
 $mailMessage = null;
+$redirect   = "http://fairsource.co.uk/#contacts";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     function sendMail()
@@ -17,9 +18,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mailFailed = true;
             $mailMessage = 'Your email address is required';
             return;
+        } 
+
+        if (empty($_POST['subject'])) {
+            $mailFailed = true;
+            $mailMessage = 'A subject is required';
+            return;
         }
 
-        if (empty($_POST['situation'])) {
+        if (empty($_POST['message'])) {
+            $mailFailed = true;
+            $mailMessage = 'A message is required';
+            return;
+        }
+
+        if (empty($_POST['name'])) {
             $mailFailed = true;
             $mailMessage = 'Your explanation for why you would like access is required';
             return;
@@ -27,11 +40,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $name = trim($_POST['name']);
         $company = trim($_POST['company']);
+        $subject = trim($_POST['subject']);
         $email = trim($_POST['email']);
-        $website = trim($_POST['website']);
+        $telephone = trim($_POST['telephone']);
+        $mess = trim($_POST['message']);
         $situation = trim($_POST['situation']);
 
-        $message = "Name: $name<br>Company: $company<br>Email: $email<br>Website: $website<br><br>$situation";
+        $message = "Name: $name<br>Email: $email<br>Phone: $telephone<br>Subject: $subject<br>Message: $mess<br>$situation";
         $from = ($name) ? $name . ' <' . $email . '>' : $email;
         $headers = "From: {$from}\r\nReply-To: {$from}\r\n";
         $headers .= "Content-type: text/html\r\n";
@@ -39,6 +54,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         if (mail($to = 'info@restructuringanonymous.com', $title, $message, $headers)) {
             $mailSuccess = true;
+            
         } else {
             $mailFailed = true;
             $mailMessage = 'Email sending failed';
@@ -46,5 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     sendMail();
+    header("Location: $redirect");
 }
+
 ?>
